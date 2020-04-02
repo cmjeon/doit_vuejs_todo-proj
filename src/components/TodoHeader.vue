394f5bcb7c0e2dda0005cf3daa3495dfd400b25e
cmjeon <cmjeon@lotte.net>
2020-04-02 11:38:58 +0900

    todoinput component

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+    <p>made bt cmj</p>
+  </header>
 </template>
 
 <script>
-export default {
-    
-};
+export default {};
 </script>
 
-<style>
+<style scoped>
+h1{
+    color:#2f3b52;
+    font-weight:900;
+    margin:2.5rem 0 1.5rem;
+}
 </style>
